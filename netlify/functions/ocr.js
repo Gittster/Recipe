88f53e76cae18dfbeeb29f4592f6be89e1a9b7ce
Matt Sweet <msweet@deltamodtech.com>
2025-04-24 11:59:33 -0500

    v4

--- netlify/functions/ocr.js
+++ netlify/functions/ocr.js
@@ -18,16 +18,14 @@ const fetch = (...args) =>
     try {
       const { base64 } = JSON.parse(event.body);
   
-      const hfResponse = await fetch("https://api-inference.huggingface.co/models/mindee/doctr-end-to-end", {
+      const hfResponse = await fetch("https://api-inference.huggingface.co/models/microsoft/trocr-base-handwritten", {
         method: "POST",
         headers: {
           "Authorization": `Bearer ${process.env.HF_API_TOKEN}`,
           "Content-Type": "application/json"
         },
         body: JSON.stringify({
-          inputs: {
-            image: base64
-          }
+          inputs: base64
         })
       });
   
